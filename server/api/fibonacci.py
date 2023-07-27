c6eb670426303ea9edc0c96f3e00723c13c12faa
vlguan <68829739+vlguan@users.noreply.github.com>
2023-07-27 00:19:44 -0700

    cleaned up fibonnaci api logic

--- server/api/fibonacci.py
+++ server/api/fibonacci.py
@@ -3,39 +3,44 @@ from flask import jsonify, request
 from api import api
 from db.db import db
 from db.models.FibonnaciModel import FibonacciNumbers
-@api.get("/fib")
 
-def calculate_fib():
-    def fibo(start, target, existingLen):
-        a, b = start[0], start[1]
-        if(a==0):
-            fib_sequence = [0,1]
-        else:    
-            fib_sequence= []
-        while len(fib_sequence) + existingLen < target:
+
+def fibo(start, target):
+        a, b = start[-2], start[-1]
+        print(a,b)
+        fib_sequence = start
+        while len(fib_sequence) < target:
             next_fib = a + b
             fib_sequence.append(next_fib)
-            a,b = b, next_fib
+            a, b = b, next_fib
         return fib_sequence
+
+@api.get("/fib")
+def calculate_fib():
     query_params = request.args.to_dict()
-    n = int(query_params['n'])
+    n = int(query_params["n"])
     fib = FibonacciNumbers.getNumbers(n)
-    final =[]
-    if(fib == []):
+    # create an array of the existing sequence
+    # pass into fibo, return whatever new sequence as an array
+    # add to db
+    # return new sequence
+    final = []
+    if fib != []:
+        for i in range(len(fib)):
+            final.append(fib[i].fib_num)
+    if fib == []:
         # no numbers in look up table
-        final = fibo([0,1], n, 0)
+        final = fibo([0, 1], n)
         for i in range(len(final)):
-            FibonacciNumbers.insertNumbers(i+1, final[i])
+            FibonacciNumbers.insertNumbers(i + 1, final[i])
+        return jsonify(final)
+    elif fib[-1].N == n:
         return jsonify(final)
     else:
-        if(fib[-1].N == n):
-            for i in fib:
-                final.append(i.fib_num)
-            return jsonify(final)
-        else:
-            # numbers in look up table but doesnt full reach n
-            temp = []
-            final=fibo([fib[-2].fib_num,fib[-1].fib_num], n, len(fib))
-            for i in fib:
-                temp.append(i.fib_num)
-            return jsonify(temp + final)
+        # numbers in look up table but doesnt full reach n
+        result = fibo(final, n)
+        # store numbers between
+        for i in range(len(fib), n):
+            FibonacciNumbers.insertNumbers(i + 1, result[i])
+        return jsonify(result)
+    
